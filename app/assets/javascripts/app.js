ed5152647d2a42f116198ed611eef09fefccaa04
JackMarx <seriousfools@gmail.com>
2017-10-16 19:25:53 -0500

    v-for

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,7 +2,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var app = new Vue({
     el: '#app',
     data: {
-      message: "Hey Vue! how's it going?"
+      message: "Hey Vue! how's it going?",
+      reviews: [
+                "I don't want to sound enamored, but I think Vue.Js is pretty cool.",
+                "Vue.js can kick react's butt",
+                "Vue.js, eh. it's ok."
+                ]
     },
     mounted: function() {
 
